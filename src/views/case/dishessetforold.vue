9fc64fb231358851aec35af224217bd8fd79b373
Penny <602140974@qq.com>
2020-09-15 16:56:50 +0800

    医生膳食

---
+++ src/views/case/dishessetforold.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="main">
+    <div class="box">
+      <el-container>
+        <el-main v-bind:class="{ 'el-mainl': true }">
+          <el-breadcrumb separator="/">
+            <el-breadcrumb-item :to="{path:'/doctor/dishadd' }">添加菜品</el-breadcrumb-item>
+            <el-breadcrumb-item :to="{path:'/doctor/dishesset'}">设置套餐</el-breadcrumb-item>
+            <el-breadcrumb-item :to="{path:'/doctor/dishessetforold'}">为老人设定套餐</el-breadcrumb-item>
+            <el-breadcrumb-item :to="{path:'/doctor/dishescalenshow'}">老人的套餐展示</el-breadcrumb-item>
+            <el-breadcrumb-item></el-breadcrumb-item>
+          </el-breadcrumb>
+        </el-main>
+        <el-main v-bind:class="{ 'el-mainr': true }">
+           <el-form
+              :model="ruleForm"
+              :rules="rules"
+              ref="ruleForm"
+              label-width="100px"
+              class="demo-ruleForm"
+            >
+              <el-form-item label="病人id">
+                <el-input v-model="form.clientId" placeholder="请输入病人id"></el-input>
+              </el-form-item>
+              <el-form-item label="套餐名">
+                <el-input v-model="form.dishSetId" placeholder="请输入套餐名"></el-input>
+              </el-form-item>
+              <el-form-item>
+                <div class="block">
+                  <div>选择起始日期</div>
+                  <span class="demonstration"></span>
+                  <el-date-picker v-model="form.startDate" type="date" placeholder="选择起始日期"></el-date-picker>
+                </div>
+                <div class="block">
+                  <div>选择终止日期</div>
+                  <span class="demonstration"></span>
+                  <el-date-picker v-model="form.endDate" type="date" placeholder="选择截止日期"></el-date-picker>
+                </div>
+              </el-form-item>
+              <el-form-item>
+                <el-button type="primary" @click.native="onsubmit">查询</el-button>
+              </el-form-item>
+            </el-form>
+        </el-main>
+      </el-container>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+     form: {
+        clientId: "",
+        dishSetId: "",
+        startDate: new Date(),
+        endDate: new Date()
+      },
+    };
+  },
+  methods: {
+    onsubmit: function() {
+      alert("success");
+    }
+  }
+};
+</script>
+<style>
+.el-mainl {
+  background-color: #ffffff;
+  color: #333;
+  text-align: center;
+  width: 30%;
+}
+.el-mainr {
+  background-color: #bdbdbd;
+  color: #333;
+  text-align: center;
+  width: 70%;
+}
+.el-container {
+  height: 600px;
+}
+</style>
--- src/views/case/docsetcase.vue
+++ src/views/case/docsetcase.vue
@@ -1,36 +1,36 @@
 <template>
   <div class="main">
     <div class="box">
-        <el-container>
-          <el-main v-bind:class="{ 'el-mainl': true }"></el-main>
-          <el-main v-bind:class="{ 'el-mainr': true }">
-            <el-form
-              :model="ruleForm"
-              :rules="rules"
-              ref="ruleForm"
-              label-width="100px"
-              class="demo-ruleForm"
-            >
-              <div>请选择护理级别</div>
-              <div class="block" style="width:500px;margin: 0 auto;">
-                <el-slider v-model="form.level" :step="1" max="3" show-stops width:100px></el-slider>
-              </div>
-              <el-form-item label="床号">
-                <el-input v-model="form.inputa" placeholder="请输入床号"></el-input>
-              </el-form-item>
-              <el-form-item label="食物需求">
-                <el-input v-model="form.inputb" placeholder="请输入食物"></el-input>
-              </el-form-item>
-              <el-form-item label="clientid">
-                <el-input v-model="form.inputc" placeholder="请输入病人id"></el-input>
-              </el-form-item>
-              <el-form-item>
-                <el-button type="primary" @click.native="onsubmit">创建</el-button>
-              </el-form-item>
-            </el-form>
-          </el-main>
-        </el-container>
-      </div>
+      <el-container>
+        <el-main v-bind:class="{ 'el-mainl': true }"></el-main>
+        <el-main v-bind:class="{ 'el-mainr': true }">
+          <el-form
+            :model="ruleForm"
+            :rules="rules"
+            ref="ruleForm"
+            label-width="100px"
+            class="demo-ruleForm"
+          >
+            <div>请选择护理级别</div>
+            <div class="block" style="width:500px;margin: 0 auto;">
+              <el-slider v-model="form.level" :step="1" max="3" show-stops width:100px></el-slider>
+            </div>
+            <el-form-item label="床号">
+              <el-input v-model="form.inputa" placeholder="请输入床号"></el-input>
+            </el-form-item>
+            <el-form-item label="食物需求">
+              <el-input v-model="form.inputb" placeholder="请输入食物"></el-input>
+            </el-form-item>
+            <el-form-item label="clientid">
+              <el-input v-model="form.inputc" placeholder="请输入病人id"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click.native="onsubmit">创建</el-button>
+            </el-form-item>
+          </el-form>
+        </el-main>
+      </el-container>
+    </div>
   </div>
 </template>
 <script>
@@ -66,8 +66,7 @@ export default {
   text-align: center;
   width: 70%;
 }
-.el-container
-{
+.el-container {
   height: 600px;
 }
 </style>
